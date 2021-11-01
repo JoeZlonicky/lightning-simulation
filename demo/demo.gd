c66153b2bb6c5a01cecfbe6e0b4e482467017f20
Joe <joe.zlonicky@gmail.com>
2021-11-01 15:58:49 -0700

    Splitting paths

---
+++ demo/demo.gd
@@ -0,0 +1,24 @@
+extends Control
+
+
+func _ready():
+	$Settings/NStrikes/NStrikesSlider.value = $Lightning.NUM_STRIKES
+	$Settings/TimeBetweenStrikes/TimeSlider.value = $Lightning.TIME_BETWEEN_STRIKES
+
+
+func _physics_process(_delta):
+	$FPSCounter.text = "FPS: " + str(Engine.get_frames_per_second())
+
+
+func _on_GenerateButton_pressed():
+	$Lightning.generate()
+
+
+func _on_NStrikesSlider_value_changed(value):
+	$Settings/NStrikes/Number.text = "(" + str(int(value)) + ")"
+	$Lightning.NUM_STRIKES = int(value)
+
+
+func _on_TimeBetweenStrikesSlider_value_changed(value):
+	$Settings/TimeBetweenStrikes/Number.text = "(" + str(value) + ")"
+	$Lightning.TIME_BETWEEN_STRIKES = value
